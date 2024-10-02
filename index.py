29232fb74c494d0486fbd1716597fcf7107e8646
franciscoj <frankmazta@gmail.com>
2024-10-02 10:57:10 -0400

    trabajando con python

---
+++ index.py
@@ -0,0 +1,20 @@
+
+
+
+# def function(palindrome): 
+#   if palindrome == palindrome[::-1]:
+#     return True
+#   else:
+#     return False
+  
+# print(function(palindrome))
+
+
+n = int(input("dame un numero"))
+
+if n > 0:
+  print(n, "es positivo")
+elif n < 0:
+  print(n, "es negativo")
+else:
+  print("n is zero ")
