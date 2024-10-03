3820c585a8aa6f7b24bd949c0c33185914607113
franciscoj <frankmazta@gmail.com>
2024-10-02 20:14:36 -0400

    funciones, set y diccionarios

--- index.py
+++ index.py
@@ -1,20 +1,59 @@
 
 
 
-# def function(palindrome): 
-#   if palindrome == palindrome[::-1]:
-#     return True
-#   else:
-#     return False
-  
-# print(function(palindrome))
+# def palindrome(txt: str):
+#   txt = txt.replace(" ", "").lower()
+#   return txt == txt[::-1]
+
+# txt_1 = "la al"
+# txt_2 = "am lo es"
+
+# print(palindrome(txt_1))
+# print(palindrome(txt_2))
 
 
-n = int(input("dame un numero"))
 
-if n > 0:
-  print(n, "es positivo")
-elif n < 0:
-  print(n, "es negativo")
-else:
-  print("n is zero ")
+# n = int(input("dame un numero"))
+
+# if n > 0:
+#   print(n, "es positivo")
+# elif n < 0:
+#   print(n, "es negativo")
+# else:
+#   print("n is zero ")
+
+# names = ["Mario","Jose","Rafael","Ana"]
+# print(names[0])
+# print()
+# names.append("Frank")
+# names.sort()
+# print(names)
+# print()
+# names.pop(0)
+# print(names)
+
+
+
+# s =set()
+
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(4)
+# s.add(5)
+# s.add(6)
+# print(s)
+# print()
+
+# s.remove(3)
+# print(s)
+# print()
+# print(f"tengo {len(s)} elemento en el set")
+
+
+
+# house = {"playa":"este","villa":"norte"}
+
+# house["campo"]= "estajero"
+
+# print(house)
