6404091dd008b05693d53a427b62d130795be578
franciscoj <frankmazta@gmail.com>
2024-10-03 12:37:19 -0400

    programación poo, uso de clase, constructor,métodos. también uso de decorador y uso del try except.

--- index.py
+++ index.py
@@ -53,7 +53,100 @@
 
 
 # house = {"playa":"este","villa":"norte"}
-
 # house["campo"]= "estajero"
-
 # print(house)
+
+
+# class Flight():
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.passengers = []
+
+#     def add_passenger(self, name):
+#         if not self.open_seats():
+#             return False
+#         self.passengers.append(name)
+#         return True
+
+
+#     def open_seats(self):
+#         return self.capacity - len(self.passengers)
+
+
+# flight = Flight(2)
+
+# people = ["Harry", "Ron", "Hermione", "Ginny"]
+
+
+# for person in people:
+#     if flight.add_passenger(person):
+#         print(f"Added {person} to flight successfully")
+#     else:
+#         print(f"No available seats for {person}")
+        
+
+
+
+# def announce(f):
+#     def wrapper():
+#         print("About to run the function")
+#         f()
+#         print("Done with the function")
+#     return wrapper
+
+# @announce
+# def hello():
+#     print("Hello, world!")
+
+# @announce
+# def goodbye():
+#     print("Goodbye, world!")
+
+# hello()
+# goodbye()
+
+# import sys
+
+# try:
+#     x = int(input("x: "))
+#     y = int(input("y: "))
+# except ValueError:
+#     print("Error: Invalid input")
+#     sys.exit(1)
+
+# try:
+#     result = x / y
+# except ZeroDivisionError:
+#     print("Error: Cannot divide by 0.")
+#     # Exit the program
+#     sys.exit(1)
+
+# print(f"{x} / {y} = {result}")
+
+# people = [
+#     {"name": "Harry", "house": "Gryffindor"},
+#     {"name": "Cho", "house": "Ravenclaw"},
+#     {"name": "Draco", "house": "Slytherin"}
+# ]
+
+# people.sort(key=lambda person: person["name"])
+
+# print(people)
+
+# import sys
+
+# try:
+#     x = int(input("x: "))
+#     y = int(input("y: "))
+# except ValueError:
+#     print("Error: Invalid input")
+#     sys.exit(1)
+
+# try:
+#     result = x / y
+# except ZeroDivisionError:
+#     print("Error: Cannot divide by 0.")
+#     # Exit the program
+#     sys.exit(1)
+
+# print(f"{x} / {y} = {result}")
